0b786270e2a55118da9b1738a01803a60547eba5
Miguelunimet <miguel.sulbaran@correo.unimet.edu.ve>
2025-11-23 13:28:27 -0400

    Cuarto commit: se creo la Ventana1, donde se fuciono la funcionalidad de CargarArchivo con nuestra interfaz, se carga el resumen y se deja apertura para empezar la siguiente ventana, donde estaran las demás funcionalidades

---
+++ CargarArchivo.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto_Estructuras2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/**
+ * Clase CargarArchivo
+ * Utilidad estática encargada de leer los archivos de texto y
+ * convertirlos en objetos de la clase Resumen tambien realiza el "parsing" 
+ * detectando las secciones del documento insertado
+ * @author Rafael Álvarez
+ */
+public class CargarArchivo {    
+    
+    /**
+     * Método Cargar
+     * Es el encargado de poder leer un archivo línea por línea e identificar
+     * las secciones del mismo en base al formato dado.
+     * @param Archivo El archivo seleccionado por el usuario.
+     * @return Un objeto Resumen listo para ser insertado o null en caso de que el 
+     * archivo no sea válido.
+     */
+    
+    public static Resumen Cargar(File Archivo) {
+        String Titulo = "";
+        StringBuilder autores = new StringBuilder();
+        StringBuilder Cuerpo_del_Resumen = new StringBuilder();
+        String palabrasClaves = "";        
+
+        boolean Leyendo_los_Autores = true;
+        boolean Leyendo_los_Resumenes = false;
+        
+        try (BufferedReader BR = new BufferedReader(new FileReader(Archivo))) {
+            String Liniesita;            
+            
+            if ((Liniesita = BR.readLine()) != null) {
+                Titulo = Liniesita.trim();
+            }
+
+            while ((Liniesita = BR.readLine()) != null) {
+                Liniesita = Liniesita.trim();
+                if (Liniesita.isEmpty()) continue; 
+                
+                if (Liniesita.equalsIgnoreCase("Resumen")) {
+                    Leyendo_los_Autores = false;
+                    Leyendo_los_Resumenes = true;
+                    continue;
+                }
+
+               
+                if (Liniesita.toLowerCase().startsWith("palabras claves:")) {
+                    Leyendo_los_Resumenes = false;                
+                    palabrasClaves = Liniesita.substring(16).trim();
+                    continue;
+                }
+                
+                if (Leyendo_los_Autores) {
+                    if (Liniesita.equalsIgnoreCase("Autores"))
+                        continue;
+                    if (autores.length() > 0) autores.append("\n");
+                    autores.append(Liniesita);
+                } else if (Leyendo_los_Resumenes) {
+                    Cuerpo_del_Resumen.append(Liniesita).append(" ");
+                }
+            }
+           
+            if (!Titulo.isEmpty()) {
+                return new Resumen(Titulo, autores.toString(), Cuerpo_del_Resumen.toString(), palabrasClaves);
+            } else {
+                return null;
+            }
+
+        } catch (IOException e) {            
+            return null;
+        }
+    }
+}
